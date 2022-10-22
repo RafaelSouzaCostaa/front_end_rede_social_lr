dd1a3beb7ff9aad1392549010005e9d10393fe72
Rafael de Souza Costa <rafaeldesouzacosta.dev@gmail.com>
2022-10-21 21:34:55 -0300

    feat: init construção da screen_login

--- lib/ScreenLogin/screen_login.dart
+++ lib/ScreenLogin/screen_login.dart
@@ -10,6 +10,71 @@ class ScreenLogin extends StatefulWidget {
 class _ScreenLoginState extends State<ScreenLogin> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 50,
+              width: 50,
+              child: Image.asset("assets/image/logo.png"),
+            ),
+            const SizedBox(height: 100),
+            Form(
+              child: Column(children: [
+                SizedBox(
+                  width: 250,
+                  height: 40,
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: "Usuario",
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 1.5,
+                          color: Color.fromARGB(26, 2, 19, 255),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                SizedBox(
+                  width: 250,
+                  height: 40,
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: "Senha",
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 1.5,
+                          color: Color.fromARGB(26, 2, 19, 255),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 50),
+                Row(
+                  children: const [
+                    Text("Não tem uma conta? "),
+                    Text("Cadastre-se"),
+                  ],
+                ),
+                const SizedBox(height: 50),
+                Row(
+                  children: [
+                    Text("Esqueceu sua senha?"),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: const Text("Login"),
+                    ),
+                  ],
+                )
+              ]),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
