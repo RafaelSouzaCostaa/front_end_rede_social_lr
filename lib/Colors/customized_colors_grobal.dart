088662c1cef712ad23f27a9d19edebaf1813e16c
Rafael de Souza Costa <rafaeldesouzacosta.dev@gmail.com>
2022-10-22 20:15:08 -0300

    feat: add ideia de cores, (validar)

--- lib/Colors/customized_colors_grobal.dart
+++ lib/Colors/customized_colors_grobal.dart
@@ -1,6 +1,15 @@
+// ignore_for_file: non_constant_identifier_names
+
 import 'package:flutter/material.dart';
 import 'package:rede_social_lr/Colors/pattern_colors.dart';
 
 class CustomizedColors {
   static Color? linkInText = PatternColors.azulBaby;
+
+  static Color? pontoFinal_EXEMPLO = PatternColors.azulBaby;
+  static Color? pontoInterrogacao_EXEMPLO = PatternColors.azulBaby;
+  static Color? botaoCancelar_EXEMPLO = PatternColors.azulBaby;
+
+  static Color? appBar_EXEMPLO = PatternColors.rozaPink;
+  static Color? botaoPadrao_EXEMPLO = PatternColors.pretoDark;
 }
---
+++ lib/Colors/pattern_colors.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class PatternColors {
+  static Color? azulBaby = const Color(0xFF26F4E8);
+  static Color? rozaPink = Color.fromARGB(255, 219, 5, 155);
+  static Color? pretoDark = Color.fromARGB(255, 28, 29, 29);
+}
