51846b5c10c80e5d46fec180f8cbb498cb23413c
Rafael de Souza Costa <rafaeldesouzacosta.dev@gmail.com>
2022-10-23 16:07:29 -0300

    refator: implementando button responsivo, new propriedades widthDouble e maxWidth

--- lib/Components/comp_button.dart
+++ lib/Components/comp_button.dart
@@ -6,13 +6,15 @@ class ComponentButton extends StatefulWidget {
   Color color;
   Function onPressed;
   double widthDouble;
+  double? maxWidth;
 
   ComponentButton(
       {super.key,
       required this.text,
       this.color = Colors.black,
       required this.onPressed,
-      this.widthDouble = 0.15});
+      this.widthDouble = 0.17,
+      this.maxWidth});
 
   @override
   State<ComponentButton> createState() => _ComponentButtonState();
@@ -21,24 +23,33 @@ class ComponentButton extends StatefulWidget {
 class _ComponentButtonState extends State<ComponentButton> {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: MediaQuery.of(context).size.width * widget.widthDouble,
-      child: ElevatedButton(
-        style: ButtonStyle(
-          shape: MaterialStateProperty.all(
-            RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
+    widget.maxWidth ??= MediaQuery.of(context).size.width * 1;
+
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+        maxWidth: widget.maxWidth!,
+      ),
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width * widget.widthDouble,
+        child: ElevatedButton(
+          style: ButtonStyle(
+            shape: MaterialStateProperty.all(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
             ),
+            backgroundColor:
+                MaterialStateProperty.all(CustomizedColors.buttonBackground),
           ),
-          backgroundColor:
-              MaterialStateProperty.all(CustomizedColors.buttonBackground),
-        ),
-        onPressed: () {
-          widget.onPressed();
-        },
-        child: Text(
-          widget.text,
-          style: TextStyle(color: widget.color, fontFamily: 'Imprima-Regular'),
+          onPressed: () {
+            widget.onPressed();
+          },
+          child: Text(
+            widget.text,
+            overflow: TextOverflow.ellipsis,
+            style:
+                TextStyle(color: widget.color, fontFamily: 'Imprima-Regular'),
+          ),
         ),
       ),
     );
