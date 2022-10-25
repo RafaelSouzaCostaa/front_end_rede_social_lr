7803a0a2028870c133db35670746d59b47746468
Luiggi <luiggi.ms.1@gmail.com>
2022-10-25 15:40:58 -0300

    Drawer design

--- lib/Components/comp_drawer.dart
+++ lib/Components/comp_drawer.dart
@@ -17,54 +17,57 @@ class _ComponentDrawerState extends State<ComponentDrawer> {
       child: SingleChildScrollView(
         child: Column(children: [
           Container(
-            padding: const EdgeInsets.only(top: 15),
+            padding: const EdgeInsets.only(top: 20, bottom: 20),
             child: const Icon(
               Icons.add_a_photo,
               color: Colors.white,
+              size: 30,
             ),
           ),
-          Text(
-            "Luiggi Rafaela, 45",
-            style: TextStyle(
-                fontSize: 30,
-                fontFamily: 'Imprima-Regular',
-                color: CustomizedColors.lightText),
-          ),
-          Text(
-            "@placeHolder",
-            style: TextStyle(
-                fontSize: 18,
-                fontFamily: 'Imprima-Regular',
-                color: CustomizedColors.lightText),
+          Container(
+            child: const Text(
+              "Luiggi Rafaela",
+              style: TextStyle(
+                  fontSize: 18,
+                  fontFamily: 'Imprima-Regular',
+                  color: Colors.white),
+            ),
           ),
           Container(
-            color: const Color.fromARGB(255, 41, 68, 39),
-            height: MediaQuery.of(context).size.height * 0.60,
+            padding: const EdgeInsets.only(top: 5, bottom: 22),
+            child: const Text(
+              "@placeHolder",
+              style: TextStyle(
+                  fontSize: 14,
+                  fontFamily: 'Imprima-Regular',
+                  color: Colors.white70),
+            ),
           ),
           Container(
-            color: const Color.fromARGB(255, 2, 29, 2),
-            height: MediaQuery.of(context).size.height * 0.10,
-            child: Container(
-              padding: const EdgeInsets.only(bottom: 30, left: 10),
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.pop(context);
-                },
+            color: Colors.white10,
+            height: MediaQuery.of(context).size.height * 0.73,
+          ),
+          Container(
+            // color: const Color.fromARGB(255, 2, 29, 2),
+            height: MediaQuery.of(context).size.height * 0.07,
+            child: TextButton(
+                onPressed: () {},
                 child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
                   children: [
                     Icon(
                       Icons.logout_outlined,
                       color: CustomizedColors.iconLogout,
                     ),
-                    const SizedBox(width: 10),
-                    Text(
-                      "LOGOUT",
-                      style: TextStyle(color: CustomizedColors.lightText),
+                    Container(
+                      padding: const EdgeInsets.only(right: 15, left: 10),
+                      child: Text(
+                        "LOGOUT",
+                        style: TextStyle(color: CustomizedColors.lightText),
+                      ),
                     ),
                   ],
-                ),
-              ),
-            ),
+                )),
           ),
         ]),
       ),
