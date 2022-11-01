af91150d7cc2bdd26179a5048c1f2d466d7f0091
Rafael de Souza Costa <rafaeldesouzacosta.dev@gmail.com>
2022-11-01 20:11:23 -0300

    sicronizando...

---
+++ lib/logic/banco_static.dart
@@ -0,0 +1,10 @@
+class bancoTestPost {
+  final post = {
+    "userName": "Joaquim Ribeiro",
+    "nickName": "JR_Bispo",
+    "userProfilePicture": "assets/image/perfil.png",
+    "numberOfLikes": 250,
+    "numberOfReposts": 10,
+    "numeberOfComments": 45
+  };
+}
