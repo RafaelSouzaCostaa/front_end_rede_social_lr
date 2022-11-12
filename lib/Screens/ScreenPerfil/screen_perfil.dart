88a245b3205d030c6edd15d4f6eba9096885dc61
Luiggi <luiggi.ms.1@gmail.com>
2022-11-12 13:20:55 -0300

    Implementação em Tela Perfil

--- lib/Screens/ScreenPerfil/screen_perfil.dart
+++ lib/Screens/ScreenPerfil/screen_perfil.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import '../../Colors/customized_colors_grobal.dart';
-import '../../Components/comp_appBar.dart';
 import '../../Components/comp_tab.dart';
 
 class ScreenPerfil extends StatefulWidget {
@@ -12,75 +11,124 @@ class ScreenPerfil extends StatefulWidget {
 }
 
 class _ScreenPerfilState extends State<ScreenPerfil> {
+  ScrollController scrollController = ScrollController();
+  bool visible = false;
+  double auxScroll = 0.0;
+
+  @override
+  void initState() {
+    scrollController = ScrollController();
+    scrollController.addListener(() {
+      if (scrollController.offset > auxScroll) {
+        auxScroll = scrollController.offset;
+      }
+
+      setState(() {
+        if (auxScroll >= 240.00) {
+          auxScroll = 0.0;
+          visible = true;
+        } else {
+          visible = false;
+        }
+      });
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: ComponentAppBar(hasDrawer: false),
-      backgroundColor: CustomizedColors.darkBackground,
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Stack(
-              children: [
-                Container(
-                  //Background image
-                  color: Colors.white,
-                  height: MediaQuery.of(context).size.height * 0.14,
-                ),
-                Container(
-                  padding: EdgeInsets.only(
-                      top: MediaQuery.of(context).size.height * 0.075),
-                  child: Center(
-                    //ATENCAO modifiquei a ideia que tava daqui para baixo, botei uma borda ao inves de 2 circle avatar
-                    child: Container(
-                      width: 110,
-                      height: 110,
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                              width: 3.2,
-                              color: CustomizedColors.darkBackground),
-                          borderRadius: BorderRadius.circular(60)),
-                      child: CircleAvatar(
-                        backgroundColor: CustomizedColors.linkInText,
-                        backgroundImage:
-                            const AssetImage("assets/image/perfil.png"),
-                        //ATENCAO Imagem de Perfil (image == null ? : ,);
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Column(
-              children: [
-                Container(
-                  padding: const EdgeInsets.only(top: 10),
-                  child: Column(children: [
-                    const Center(
-                      child: Text(
-                        "Nome do Usuario", //NomePerfil
-                        style: TextStyle(
+    //double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: CustomizedColors.darkBackground,
+        body: CustomScrollView(
+          controller: scrollController,
+          slivers: [
+            SliverAppBar(
+              backgroundColor: CustomizedColors.darkBackground,
+              pinned: true,
+              expandedHeight: 300,
+              leading: visible
+                  ? Text("ssds", style: TextStyle(color: Colors.amber))
+                  : null,
+              title: visible
+                  ? Text("data", style: TextStyle(color: Colors.amber))
+                  : null,
+              actions: visible
+                  ? [Text("action", style: TextStyle(color: Colors.amber))]
+                  : null,
+              flexibleSpace: FlexibleSpaceBar(
+                background: Column(
+                  children: [
+                    Stack(
+                      children: [
+                        Container(
+                          //Background image
                           color: Colors.white,
-                          fontSize: 20,
-                          fontFamily: 'Imprima-Regular',
+                          height: MediaQuery.of(context).size.height * 0.20,
                         ),
-                      ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.only(top: 5),
-                      child: Center(
-                        child: Text(
-                          "@nickname", //NomePerfil
-                          style: TextStyle(
-                            color: CustomizedColors.linkInText,
-                            fontSize: 14,
-                            fontFamily: 'Imprima-Regular',
+                        Container(
+                          padding: EdgeInsets.only(
+                              top: MediaQuery.of(context).size.height * 0.075),
+                          child: Center(
+                            //ATENCAO modifiquei a ideia que tava daqui para baixo, botei uma borda ao inves de 2 circle avatar
+                            child: Container(
+                              width: 110,
+                              height: 110,
+                              decoration: BoxDecoration(
+                                  border: Border.all(
+                                      width: 3.2,
+                                      color: CustomizedColors.darkBackground),
+                                  borderRadius: BorderRadius.circular(60)),
+                              child: CircleAvatar(
+                                backgroundColor: CustomizedColors.linkInText,
+                                backgroundImage:
+                                    const AssetImage("assets/image/perfil.png"),
+                                //ATENCAO Imagem de Perfil (image == null ? : ,);
+                              ),
+                            ),
                           ),
                         ),
-                      ),
+                      ],
                     ),
-                  ]),
+                    Text(
+                      "Nome",
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                    ),
+                    Text(
+                      "Nickname",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    Text(
+                      "Descrição akjdfaskjdghsakjdgakshdglhafghasdfjshdhjsad" +
+                          "klhsafdhgashdgsahdgsadgalsgdjsadsadsagdlhsagjd",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    Container(
+                      color: Colors.pink,
+                      width: screenWidth * 0.15,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "X Seguindo",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          Text(
+                            "X Seguidores",
+                            style: TextStyle(color: Colors.white),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
                 ),
+              ),
+            ),
+            SliverList(
+              delegate: SliverChildListDelegate([
                 ComponentTab(
                   height: 0.56,
                   width: 1,
@@ -100,7 +148,7 @@ class _ScreenPerfilState extends State<ScreenPerfil> {
                     ),
                   ],
                 ),
-              ],
+              ]),
             ),
           ],
         ),
--- lib/Screens/screen_teste.dart
+++ lib/Screens/screen_teste.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../Colors/customized_colors_grobal.dart';
-import '../Components/comp_tab.dart';
-
 class Testes extends StatefulWidget {
   const Testes({super.key});
 
@@ -11,109 +8,13 @@ class Testes extends StatefulWidget {
 }
 
 class _TestesState extends State<Testes> {
-  ScrollController scrollController = ScrollController();
-  bool visible = false;
-  double auxScroll = 0.0;
-
-  @override
-  void initState() {
-    scrollController = ScrollController();
-    scrollController.addListener(() {
-      if (scrollController.offset > auxScroll) {
-        auxScroll = scrollController.offset;
-      }
-
-      setState(() {
-        if (auxScroll >= 240.00) {
-          auxScroll = 0.0;
-          visible = true;
-        } else {
-          visible = false;
-        }
-      });
-    });
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
+    //double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+
     return SafeArea(
-      child: Scaffold(
-        body: CustomScrollView(
-          controller: scrollController,
-          slivers: [
-            SliverAppBar(
-              pinned: true,
-              expandedHeight: 300,
-              leading: visible
-                  ? Text("ssds", style: TextStyle(color: Colors.amber))
-                  : null,
-              title: visible
-                  ? Text("data", style: TextStyle(color: Colors.amber))
-                  : null,
-              actions: visible
-                  ? [Text("action", style: TextStyle(color: Colors.amber))]
-                  : null,
-              flexibleSpace: FlexibleSpaceBar(
-                background: Stack(
-                  children: [
-                    Container(
-                      //Background image
-                      color: Colors.white,
-                      height: MediaQuery.of(context).size.height * 0.20,
-                    ),
-                    Container(
-                      padding: EdgeInsets.only(
-                          top: MediaQuery.of(context).size.height * 0.075),
-                      child: Center(
-                        //ATENCAO modifiquei a ideia que tava daqui para baixo, botei uma borda ao inves de 2 circle avatar
-                        child: Container(
-                          width: 110,
-                          height: 110,
-                          decoration: BoxDecoration(
-                              border: Border.all(
-                                  width: 3.2,
-                                  color: CustomizedColors.darkBackground),
-                              borderRadius: BorderRadius.circular(60)),
-                          child: CircleAvatar(
-                            backgroundColor: CustomizedColors.linkInText,
-                            backgroundImage:
-                                const AssetImage("assets/image/perfil.png"),
-                            //ATENCAO Imagem de Perfil (image == null ? : ,);
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            SliverList(
-              delegate: SliverChildListDelegate([
-                ComponentTab(
-                  height: 0.56,
-                  width: 1,
-                  indicatorColor: CustomizedColors.blueBackground,
-                  labelColor: CustomizedColors.blueBackground,
-                  unselectedLabelColor: CustomizedColors.lightText,
-                  tabsName: const [
-                    Text("Postagens"),
-                    Text("Curtidas"),
-                  ],
-                  tabs: [
-                    Tab(
-                      child: Container(color: Colors.transparent),
-                    ),
-                    Tab(
-                      child: Container(color: Colors.transparent),
-                    ),
-                  ],
-                ),
-              ]),
-            ),
-          ],
-        ),
-      ),
+      child: Scaffold(),
     );
   }
 }
