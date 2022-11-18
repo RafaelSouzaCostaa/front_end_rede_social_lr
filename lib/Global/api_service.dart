61f9edc95c3b1723c8d8df991ae74b9146980043
Luiggi <luiggi.ms.1@gmail.com>
2022-11-18 15:28:35 -0300

    Observable token

--- lib/Global/api_service.dart
+++ lib/Global/api_service.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../Models/model_profile.dart';
 import 'api_constants.dart';
+import 'token.dart';
 
 class ApiService {
   static Future<Profile> createProfile(Profile profile) async {
@@ -21,6 +22,24 @@ class ApiService {
     );
     if (response.statusCode == 200) {
       return Profile.fromMap(jsonDecode(response.body));
+    } else {
+      throw Exception('Falha ao criar Perfil');
+    }
+  }
+
+  static Future<Token> login(String email, String password) async {
+    final response = await http.post(
+        Uri.parse(APIConstants.apiUrl + APIConstants.createProfile),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(<String, String>{
+          'email': email,
+          'password': password,
+        }));
+    if (response.statusCode == 200) {
+      Token token = new Token.obs();//RAFAEL ignora aqui por enquanto
+      return token;
     } else {
       throw Exception('Falha ao criar Perfil');
     }
--- lib/Global/token.dart
+++ lib/Global/token.dart
@@ -1,18 +1,11 @@
 import 'package:get/get.dart';
 
-class TokenController extends GetxController {
-  late String _token;
+class Token extends GetxController {
+  late String _token = List.obs; //RAFAEL faz uma lista aqui
 
   get token => _token;
 
-  void criarToken() {}
-
   void apagarToken() {
     _token = "";
-  }
-
-  void verificarToken() {
-    //LUIGGI oque seria essa verificarToken?
-    //não precisa disso, só implementar um singleton na criação do token
   }
 }
