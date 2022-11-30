4797766254fc7f1a9a3193603dee43bd5a8b5e44
Rafael de Souza Costa <rafaeldesouzacosta.dev@gmail.com>
2022-11-30 13:45:16 -0300

    refactor: inuteis

--- lib/Components/comp_post.dart
+++ lib/Components/comp_post.dart
@@ -34,116 +34,113 @@ class ComponentPost extends StatefulWidget {
 class _ComponentPostState extends State<ComponentPost> {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Column(
-                children: [
-                  Container(
-                    padding: const EdgeInsets.only(top: 10, left: 15),
-                    child: CircleAvatar(
-                      radius: 20,
-                      backgroundColor: CustomizedColors.blueBackground,
-                      //IMPLEMENTAR trocar foto pelo do perfil quando implementar
-                      child: const Icon(
-                        Icons.add_a_photo,
-                        size: 15,
-                      ),
+    return Column(
+      children: [
+        Row(
+          children: [
+            Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(top: 10, left: 10),
+                  child: CircleAvatar(
+                    radius: 20,
+                    backgroundColor: CustomizedColors.blueBackground,
+                    //IMPLEMENTAR trocar foto pelo do perfil quando implementar
+                    child: const Icon(
+                      Icons.add_a_photo,
+                      size: 15,
                     ),
                   ),
-                ],
-              ),
-              Column(
-                children: [
-                  Row(
-                    //Nome Nickname e opções
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.only(left: 10),
-                        width: Get.width * 0.74,
-                        child: Row(
-                          children: [
-                            Flexible(
-                              child: ComponentText(
-                                text: widget.postUsername,
-                              ),
-                            ),
-                            Flexible(
-                              child: ComponentText(
-                                  text: widget.postNickname,
-                                  color: Colors.grey),
-                            ),
-                          ],
-                        ),
-                      ),
-                      IconButton(
-                        icon: const Icon(Icons.more_vert),
-                        onPressed: () {},
-                      )
-                    ],
-                  ),
-                  //imagens do Post
-                ],
-              ),
-            ],
-          ),
-          //Uma imagem
-          Container(
-            padding: const EdgeInsets.only(left: 60),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                widget.postImage?.length == 1
-                    ? Container(
-                        color: Colors.pink,
-                        width: Get.width * 0.75,
-                        height: 300,
-                      )
-                    : Container(),
-                //Duas imagens
-                widget.postImage?.length == 2
-                    ? Row(
-                        children: [
-                          Container(
-                            color: Colors.green,
-                            width: Get.width * 0.38,
-                            height: 300,
-                          ),
-                          const SizedBox(
-                            width: 5,
-                          ),
-                          Container(
-                            color: Colors.blue,
-                            width: Get.width * 0.38,
-                            height: 300,
-                          )
-                        ],
-                      )
-                    : Container(),
-                //Três imagens
-                //LUIGGI FAZER 3 e 4
-                widget.postImage?.length == 3
-                    ? Container(
-                        color: Colors.pink,
-                        width: Get.width * 0.8,
-                        height: 300,
-                      )
-                    : Container(),
-                //Quatro imagens
-                widget.postImage?.length == 4
-                    ? Container(
-                        color: Colors.pink,
-                        width: Get.width * 0.8,
-                        height: 300,
-                      )
-                    : Container(),
+                ),
               ],
             ),
-          )
-        ],
-      ),
+            Column(
+              children: [
+                Row(
+                  //Nome Nickname e opções
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.only(left: 10),
+                      width: Get.width * 0.74,
+                      child: Row(
+                        children: [
+                          Flexible(
+                            child: ComponentText(
+                              text: widget.postUsername,
+                            ),
+                          ),
+                          Flexible(
+                            child: ComponentText(
+                                text: widget.postNickname, color: Colors.grey),
+                          ),
+                        ],
+                      ),
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.more_vert),
+                      onPressed: () {},
+                    )
+                  ],
+                ),
+                //imagens do Post
+              ],
+            ),
+          ],
+        ),
+        //Uma imagem
+        Container(
+          padding: const EdgeInsets.only(left: 60),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              widget.postImage?.length == 1
+                  ? Container(
+                      color: Colors.pink,
+                      width: Get.width * 0.75,
+                      height: 300,
+                    )
+                  : Container(),
+              //Duas imagens
+              widget.postImage?.length == 2
+                  ? Row(
+                      children: [
+                        Container(
+                          color: Colors.green,
+                          width: Get.width * 0.38,
+                          height: 300,
+                        ),
+                        const SizedBox(
+                          width: 5,
+                        ),
+                        Container(
+                          color: Colors.blue,
+                          width: Get.width * 0.38,
+                          height: 300,
+                        )
+                      ],
+                    )
+                  : Container(),
+              //Três imagens
+              //LUIGGI FAZER 3 e 4
+              widget.postImage?.length == 3
+                  ? Container(
+                      color: Colors.pink,
+                      width: Get.width * 0.8,
+                      height: 300,
+                    )
+                  : Container(),
+              //Quatro imagens
+              widget.postImage?.length == 4
+                  ? Container(
+                      color: Colors.pink,
+                      width: Get.width * 0.8,
+                      height: 300,
+                    )
+                  : Container(),
+            ],
+          ),
+        )
+      ],
     );
   }
 }
