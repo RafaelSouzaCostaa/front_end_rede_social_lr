7b8c95a1245bf69fc9e4dac06a4c10420009af2d
Luiggi <luiggi.ms.1@gmail.com>
2022-12-05 15:22:41 -0300

    Novas rotas da api em Api Constansts

--- lib/Global/api_constants.dart
+++ lib/Global/api_constants.dart
@@ -1,32 +1,36 @@
 class APIConstants {
-  static String apiUrl = 'http://177.6.193.211:3000';
+  // static String apiUrl = 'http://177.6.193.211:3000';
+  static String apiUrl = 'http://localhost:3000';
 
   //endpoints
   static String login = '/login/';
 
   //Profile
   static String createProfile = '/profile/create';
+
   static String getAllProfiles = '/profile/getAll';
   static String getProfileByToken = '/profile/getProfileByToken';
+  static String getProfileById(id) {
+    return '/profile/getById/$id';
+  }
 
   static String getProfileByNickname(nickname) {
     return '/profile/getByNickname/$nickname';
   }
 
-  static String getProfileById(id) {
-    return '/profile/getById/$id';
+  static String followProfile(id) {
+    return '/profile/follow/$id';
   }
+
+  static String updateProfile = '/profile/updateById';
 
   static String deleteProfileById(id) {
     return '/profile/deleteById/$id';
   }
 
-  static String setFollowProfile(id) {
-    return '/profile/setFollow/$id';
-  }
-
   //Posts
   static String createPost = '/post/create';
+  static String likePost = '/post/like';
   static String getAllPosts = '/post/getAll';
   static String getAllByFollow = '/post/getAllByFollow';
 
@@ -38,7 +42,24 @@ class APIConstants {
     }
   }
 
+  static String deletePostById(profileId, postId) {
+    return '/post/deleteById/$profileId/$postId';
+  }
+
   //Comments
-  static String createComment = '/comment/create';
-  static String createSubComment = '/comment/createSubComment';
+  static String createComment(profileId) {
+    return '/comment/create/$profileId';
+  }
+
+  static String createSubComment(profileId) {
+    return '/comment/createSubComment/$profileId';
+  }
+
+  static String deleteCommentById(profileId, commentId) {
+    return '/comment/deleteById/$profileId/$commentId';
+  }
+
+  static String deleteSubCommentById(profileId, commentId, subCommentId) {
+    return '/comment/deleteSubComment/$profileId/$commentId/$subCommentId';
+  }
 }
