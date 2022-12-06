265d51c6e472c2cb57dba9d06e5662e45f8e1c28
Rafael de Souza Costa <rafaeldesouzacosta.dev@gmail.com>
2022-12-06 16:29:27 -0300

    feat: add base64 integrado combanco de dados, e pagina de criar posts

--- lib/Screens/ScreenPost/screen_create_post.dart
+++ lib/Screens/ScreenPost/screen_create_post.dart
@@ -1,5 +1,13 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:rede_social_lr/Global/api_service.dart';
+import 'package:flutter_image_compress/flutter_image_compress.dart';
 
 import '../../Components/comp_app_bar.dart';
 import '../../Components/comp_button.dart';
@@ -15,56 +23,126 @@ class ScreenCreatePost extends StatefulWidget {
 class _ScreenCreatePostState extends State<ScreenCreatePost> {
   final TextEditingController _textController = TextEditingController();
 
+  List<String> listImage = List.empty(growable: true);
+
+  addImage(String imageEncode) {
+    listImage.add(imageEncode);
+  }
+
+  Future<File> processImageCamera(XFile pickedFile) async {
+    File img = File(pickedFile.path);
+    Directory directory = await getApplicationDocumentsDirectory();
+    String localPath = directory.path;
+    String uniqueID = UniqueKey().toString();
+
+    return await img.copy('$localPath/image_$uniqueID.png');
+  }
+
+  Future<String> imageCompressEncodeBase64(File image) async {
+    Uint8List imageBytes = image.readAsBytesSync();
+
+    Uint8List compress =
+        await FlutterImageCompress.compressWithList(imageBytes, quality: 50);
+
+    return base64Encode(compress);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const ComponentAppBar(),
       body: Center(
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Container(
-                  padding: const EdgeInsets.only(top: 15, right: 15),
-                  child: ComponentButton(
-                    height: 7,
-                    width: 20,
-                    textStyle: const TextStyle(
-                      fontWeight: FontWeight.w700,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.only(top: 15, right: 15),
+                    child: ComponentButton(
+                      height: 7,
+                      width: 22,
+                      textStyle: const TextStyle(
+                        fontWeight: FontWeight.w700,
+                      ),
+                      text: "Kitterar",
+                      onPressed: () {
+                        if (listImage.isNotEmpty &&
+                            _textController.text != "") {
+                          ApiService.createPost(
+                              _textController.text, listImage);
+                        }
+                      },
                     ),
-                    text: "Kitterar",
-                    onPressed: () {},
+                  ),
+                ],
+              ),
+              const Divider(
+                color: Colors.white,
+                thickness: 0.2,
+              ),
+              if (listImage.isNotEmpty)
+                SizedBox(
+                  width: Get.width - 10,
+                  height: Get.height * 0.6,
+                  child: ListView.builder(
+                    itemCount: listImage.length,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return Image(
+                        image: MemoryImage(
+                          Uint8List.fromList(
+                            base64Decode(
+                              listImage[index],
+                            ),
+                          ),
+                        ),
+                      );
+                    },
                   ),
                 ),
-              ],
-            ),
-            const Divider(
-              color: Colors.white,
-              thickness: 0.2,
-            ),
-            Container(
-              padding: const EdgeInsets.only(top: 10),
-              child: ComponentInput(
-                hintText: 'anygossip'.tr,
-                controller: _textController,
-                focusedBorderColor: Colors.transparent,
-                unfocusedBorderColor: Colors.transparent,
-                maxLines: 5,
-                minLines: 5,
-                maxLenght: 280,
-                fontSize: 19,
-                width: Get.width,
+              GestureDetector(
+                child: const Icon(Icons.add_a_photo),
+                onTap: () async {
+                  final ImagePicker picker = ImagePicker();
+                  final XFile? pickedFile =
+                      await picker.pickImage(source: ImageSource.camera);
+                  if (pickedFile != null) {
+                    File image = await processImageCamera(pickedFile);
+                    String imgEncode = await imageCompressEncodeBase64(image);
+                    addImage(imgEncode);
+                  }
+                  setState(() {});
+                },
               ),
-            ),
-            const Divider(
-              color: Colors.white,
-              thickness: 0.2,
-            ),
-            Row(
-              children: [],
-            )
-          ],
+              const Divider(
+                color: Colors.white,
+                thickness: 0.2,
+              ),
+              Container(
+                padding: const EdgeInsets.only(top: 10),
+                child: ComponentInput(
+                  hintText: 'anygossip'.tr,
+                  controller: _textController,
+                  focusedBorderColor: Colors.transparent,
+                  unfocusedBorderColor: Colors.transparent,
+                  maxLines: 5,
+                  minLines: 5,
+                  maxLenght: 280,
+                  fontSize: 19,
+                  width: Get.width,
+                ),
+              ),
+              const Divider(
+                color: Colors.white,
+                thickness: 0.2,
+              ),
+              Row(
+                children: [],
+              )
+            ],
+          ),
         ),
       ),
     );
